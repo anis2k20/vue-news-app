76a948f783868a91bb799e585e81474cc5d380d4
Anis <anis2k20@gmail.com>
2024-07-11 19:45:53 +0600

    link fixed

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -6,7 +6,7 @@
         <img :src="n.urlToImage" alt="photo" class="rounded-md">
       </div>
       <h3 class="font-semibold text-primary">{{ n.title.slice(0, 60) }}</h3>
-      <p>{{ n.description?.slice(0, 40) }} <a class="font-bold py-2 cursor-pointer hover:text-primary" href="#">...Read
+      <p>{{ n.description?.slice(0, 40) }} <a class="font-bold py-2 cursor-pointer hover:text-primary" :href="n.url">...Read
         More</a></p>
     </div>
     <!--  end card-->
